9eb4a3ceaac096edcef9fb40a9d760110ca00ee7
Michael Lynch <mtlynch@users.noreply.github.com>
2017-10-16 23:09:35 -0400

    Refactoring downloader to separate module (#4)

--- tests/test_downloader.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from downloader import downloader
-
-
-class DownloaderTest(unittest.TestCase):
-
-    def test_dummy(self):
-        downloader.dummy()
---
+++ tests/test_url.py
@@ -0,0 +1,43 @@
+import unittest
+import urllib2
+
+import mock
+
+from downloader import url
+
+
+class ImagesTest(unittest.TestCase):
+
+    def setUp(self):
+        mock_urlopen_patch = mock.patch(
+            'downloader.url.urllib2.urlopen', autospec=True)
+        self.addCleanup(mock_urlopen_patch.stop)
+        self.mock_urlopen = mock_urlopen_patch.start()
+
+        mock_request_patch = mock.patch(
+            'downloader.url.urllib2.Request', autospec=True)
+        self.addCleanup(mock_request_patch.stop)
+        self.mock_request = mock_request_patch.start()
+
+    def test_download_succeeds_when_server_is_ok(self):
+        mock_handle = mock.Mock()
+        mock_handle.info.return_value = mock.Mock(type='image/jpeg')
+        mock_handle.read.return_value = 'dummy image data'
+        self.mock_urlopen.return_value = mock_handle
+        self.assertEqual(
+            url.download_image_data('http://mock.com/image.jpg'),
+            'dummy image data')
+
+    def test_download_fails_when_server_returns_403(self):
+        self.mock_urlopen.side_effect = urllib2.HTTPError(
+            url='', code=404, msg='', hdrs=None, fp=None)
+        with self.assertRaises(urllib2.HTTPError):
+            url.download_image_data('http://mock.com/image.jpg')
+
+    def test_download_fails_when_content_type_is_not_jpeg(self):
+        mock_handle = mock.Mock()
+        mock_handle.info.return_value = mock.Mock(type='image/png')
+        mock_handle.read.return_value = 'dummy image data'
+        self.mock_urlopen.return_value = mock_handle
+        with self.assertRaises(url.UnexpectedImageType):
+            url.download_image_data('http://mock.com/image.jpg')
