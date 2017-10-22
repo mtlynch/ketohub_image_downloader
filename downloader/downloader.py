4c790077f73cef3b81a85d00e5ee8f4bf8f80f81
Michael Lynch <mtlynch@users.noreply.github.com>
2017-10-22 14:35:36 -0400

    Switching to jobs library (#10)

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -6,6 +6,7 @@ import time
 import urllib2
 
 import input_file
+import jobs
 import url_downloader
 
 logger = logging.getLogger(__name__)
@@ -32,46 +33,39 @@ def _write_to_file(filepath, content):
     open(filepath, 'wb').write(content)
 
 
-def _download_image_urls(url_dict, output_root):
+def _process_download_jobs(job_list):
     download_queue = Queue.Queue()
+    for job in job_list:
+        if not os.path.exists(job.output_path()):
+            download_queue.put(job)
 
-    for key, url in url_dict.iteritems():
-        output_filename = key + '.jpg'
-        output_path = os.path.join(output_root, output_filename)
-        if os.path.exists(output_path):
-            continue
-        download_queue.put({
-            'url': url,
-            'destination': output_path,
-            'attempts': 0
-        })
     logger.info('%d URLs do not exist locally', download_queue.qsize())
     download_delay = 2.0
     while not download_queue.empty():
-        logger.info('%d items left in queue', download_queue.qsize())
-        item = download_queue.get()
-        if item['attempts'] > 5:
+        logger.info('%d jobs left in queue', download_queue.qsize())
+        job = download_queue.get()
+        if job.attempts() > 5:
             logger.error('Tried to download %s %d times, giving up.',
-                         item['url'], item['attempts'])
+                         job.url(), job.attempts())
             continue
-        logger.info('Downloading %s', item['url'])
+        logger.info('Downloading %s', job.url())
 
         try:
-            _write_to_file(item['destination'],
-                           url_downloader.download_image_data(item['url']))
+            _write_to_file(job.output_path(),
+                           url_downloader.download_image_data(job.url()))
             download_delay = max(download_delay - 1.0, 2.0)
         except urllib2.HTTPError as e:
-            logger.warn('Got error trying to download %s: %s', item['url'], e)
+            logger.warn('Got error trying to download %s: %s', job.url(), e)
             code_family = (e.code / 100) * 100
             if (code_family == 400) or (code_family == 500):
-                item['attempts'] += 1
-                download_queue.put(item)
+                job.record_attempt()
+                download_queue.put(job)
                 download_delay = min(30.0, download_delay * 1.75)
             else:
                 raise
         except url_downloader.UnexpectedImageType as e:
             logger.warn('Unexpected image trying to download %s: %s',
-                        item['url'], e)
+                        job.url(), e)
         time.sleep(download_delay)
 
 
@@ -80,7 +74,8 @@ def main(args):
     with open(args.input_file) as input_file_handle:
         url_dict = input_file.parse(input_file_handle)
     logger.info('Read %d input URLs', len(url_dict))
-    _download_image_urls(url_dict, args.output_root)
+    job_list = jobs.from_url_dict(url_dict, args.output_root)
+    _process_download_jobs(job_list)
 
 
 if __name__ == '__main__':
