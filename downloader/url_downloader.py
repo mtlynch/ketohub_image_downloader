18423f707d9325ae84ff46a6128af4b59ce8dffe
Michael Lynch <mtlynch@users.noreply.github.com>
2018-01-03 20:51:40 -0500

    Fixing image downloader to support UTF-8 encoded URL paths (#15)

--- downloader/url_downloader.py
+++ downloader/url_downloader.py
@@ -1,4 +1,5 @@
 import urllib2
+import urlparse
 
 _USER_AGENT = (
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -15,8 +16,17 @@ class UnexpectedImageType(Error):
 
 def download_image_data(image_url):
     image_handle = urllib2.urlopen(
-        urllib2.Request(image_url, headers={'User-Agent': _USER_AGENT}))
+        urllib2.Request(
+            _encode_url(image_url), headers={'User-Agent': _USER_AGENT}))
     if image_handle.info().type not in ['image/jpeg', 'image/png']:
         raise UnexpectedImageType('Unexpected image type: ' +
                                   image_handle.info().type)
     return image_handle.read()
+
+
+def _encode_url(url):
+    scheme, network_location, path, query, fragment_identifier = urlparse.urlsplit(
+        url)
+    path = urllib2.quote(path.encode('utf8'))
+    return urlparse.urlunsplit((scheme, network_location, path, query,
+                                fragment_identifier))
