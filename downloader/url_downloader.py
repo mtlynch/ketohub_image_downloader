97e683d33891657d771d6c3a2d4afb2e4318a39e
Michael Lynch <michael@ketohub.io>
2018-05-30 08:35:46 -0400

    Ignoring SSL checks for image download

--- downloader/url_downloader.py
+++ downloader/url_downloader.py
@@ -1,3 +1,4 @@
+import ssl
 import urllib2
 import urlparse
 
@@ -17,11 +18,24 @@ class UnexpectedImageType(Error):
 def download_image_data(image_url):
     image_handle = urllib2.urlopen(
         urllib2.Request(
-            _encode_url(image_url), headers={'User-Agent': _USER_AGENT}))
+            _encode_url(image_url), headers={'User-Agent': _USER_AGENT}),
+        context=_get_insecure_ssl_context())
     if image_handle.info().type not in ['image/jpeg', 'image/png']:
         raise UnexpectedImageType('Unexpected image type: ' +
                                   image_handle.info().type)
     return image_handle.read()
+
+
+def _get_insecure_ssl_context():
+    """Creates a no-verify SSL context.
+
+    Since we're just downloading images, we'd rather blindly accept bad
+    certificates than fail when a site has bad SSL configurations.
+    """
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+    return context
 
 
 def _encode_url(url):
