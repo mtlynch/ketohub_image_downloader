27dfa30f0da5dfe871db86e8142e345317354e8a
Michael Lynch <mtlynch@users.noreply.github.com>
2019-04-23 08:28:39 -0500

    Updating bash scripts (#21)
    * Updating bash scripts
    
    * Backing out environment variable option from build script

--- third_party/docstringchecker/update.sh
+++ third_party/docstringchecker/update.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Downloads the latest copy of the DocStringChecker plugin.
 
-# Exit build script on first failure
+# Exit build script on first failure.
 set -e
 
 # Echo commands to stdout.
 set -x
+
+# Treat undefined variables as errors.
+set -u
 
 OUTPUT_DIR=$(dirname "$0")
 OUTPUT_FILE="${OUTPUT_DIR}/lint.py"
